a78d4fe49ae45cb5c99c8e753709ae274fc48fbd
Chris Smith <chris.smith@xerox.com>
2022-02-02 20:47:49 -0800

    reorganize directories

---
+++ content/js/firebase-credentials.js
@@ -0,0 +1,18 @@
+window.ta_config_ui_firebase_credentials = {
+    "apiKey": "AIzaSyD_HuUx31rUWZlNW3MuhFUYCgJMST6qb88",
+    "authDomain": "ta-central-ws.firebaseapp.com",
+    "projectId": "ta-central-ws",
+    "storageBucket": "ta-central-ws.appspot.com",
+    "messagingSenderId": "599699181516",
+    "appId": "1:599699181516:web:4d4886450342754da2d769"
+};
+
+window.ta_config_ui_firebase_credentials = {
+    "apiKey": "AIzaSyBWxHcjAseUpnuud3N0cnajWmAVqA9HtQE",
+    "authDomain": "transit-appliance-config.firebaseapp.com",
+    "projectId": "transit-appliance-config",
+    "storageBucket": "transit-appliance-config.appspot.com",
+    "messagingSenderId": "881008083854",
+    "appId": "1:881008083854:web:464b10e392bd097f2edadf",
+    "measurementId": "G-SDYPNVDTPJ"
+};
