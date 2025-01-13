c148ab4fa5a16a4aa768d6295dd2d8493fc3e3f3
Chris Smith <chris.smith@xerox.com>
2025-01-12 20:52:39 -0800

    continue reworking geocoder

--- content/js/trAppRegistration.js
+++ content/js/trAppRegistration.js
@@ -97,7 +97,8 @@ async function trAppGeocodePrivateApplianceConfig() {
 		if (trApp.current_appliance['private']['address'] != trApp.current_appliance['private']['mapped_address']) {
 			//var geocoder = new google.maps.Geocoder();
 			var geocoder = await google.maps.importLibrary("geocoding");
-		  geocoder.geocode( { 'address': trApp.current_appliance['private']['address'] }, function(results, status) {
+		  	// geocoder.geocode( { 'address': trApp.current_appliance['private']['address'] }, function(results, status) {
+			geocoder.geocode( geocode(request: google.maps.GeocoderRequest({ 'address': trApp.current_appliance['private']['address'] })), function(results, status) {
 		    if (status == google.maps.GeocoderStatus.OK) {
 		      var latlng = results[0].geometry.location;
 		      trApp.current_appliance['private']['lat'] = latlng.lat();
