1093f1e6cf47235cf3d5c746f0a62f8852873af0
Chris Smith <chris.smith@xerox.com>
2025-10-12 15:07:20 -0700

    add redirect dev

--- content/js/trApplicationCache.js
+++ content/js/trApplicationCache.js
@@ -765,6 +765,39 @@ var trStoredApplications = [
       }
     ]
   },
+  {
+    "spacer": true,
+    "title": "Ancillary apps (development)",
+    "application_id": "ancillary_dev"
+    // development
+  },
+  {
+    "description": "Displays arrivals grouped by transit line, with additional info on the right",
+    "agencies": [
+      "TriMet",
+      "Portland Streetcar"
+    ],
+    "title": "Redirect (development)",
+    "application_id": "redirect_dev",
+    "_id": "redirect_dev",
+    "templates": [
+      {
+        "img_url": "http://dev.transitappliance.com/apps/assets/img/ping.png",
+        "app_url": "http://dev.transitappliance.com/apps/redirect/app.html?${multi_agency_stop_string}&${application.fully_qualified_option_string}&option[message]=+&appl[id]=${id}&appl[timezone]=${timezone}"
+      }
+    ],
+    "url_template": "http://dev.transitappliance.com/apps/redirect/app.html?appl[id]=${id}&appl[timezone]=${timezone}&${multi_agency_stop_string}&${application.fully_qualified_option_string}",
+    "fields": [
+     
+
+      {
+        "label": "Redirect Configuration ID:",
+        "advice": "Identifier for configuration to redirect to",
+        "html": "\u003Cinput type='text' name='redirect' value='' size='45'\u003E"
+      }
+
+    ]
+  },
  
 ];
 
